5d8d266f0d97b62d1e2578d8ec50eb260af02e0e
m4rkbello <markbello10041999@gmail.com>
2024-02-21 23:59:27 +0800

    register authentication with LARAVEL SANCTUM (Token)

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+
+        public function __construct(){
+            $this->middleware("auth:sanctum",['except'=>["register"]]);
+        }
+
+
+    public function register(Request $request){
+
+        $request->validate([
+            "name"=>'required|string',
+            "email"=>"required|unique:users,email",
+            "password"=>"required|string|confirmed",
+        ]);
+
+        $user=User::create([
+            "name"=>$request->name,
+            "email"=>$request->email,
+            "password"=>bcrypt($request->password),
+        ]);
+
+        $token=$user->createToken("token-name")->plainTextToken;
+
+        return response()->json([
+            "success"=>$user,
+            "token"=>$token,
+        ]);
+    }
 }
