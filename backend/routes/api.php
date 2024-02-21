78c1026f1359f996fd29d6e4d12c9482581bfe56
m4rkbello <markbello10041999@gmail.com>
2024-02-22 00:51:38 +0800

    WORKING

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::post('Register', [AuthController::class, "register"]);
+Route::post('register', [AuthController::class, "register"]);
