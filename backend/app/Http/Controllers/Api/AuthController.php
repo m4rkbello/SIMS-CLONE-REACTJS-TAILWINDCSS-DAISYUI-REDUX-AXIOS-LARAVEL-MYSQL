25e1c3f6233d6149223d82f080fec0b472efbf6f
m4rkbello <markbello10041999@gmail.com>
2024-02-22 22:21:32 +0800

    ADD API ROUTES FOR AUTH -SANCTUM IN LOGIN

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; 
 
 class AuthController extends Controller
 {
 
         public function __construct(){
-            $this->middleware("auth:sanctum",['except'=>["register"]]);
+            $this->middleware("auth:sanctum",['except'=>["register","login"]]);
         }
 
 
@@ -42,9 +43,29 @@ class AuthController extends Controller
             "success"=>true,
             "message"=>"User has been log Out!"
         ]);
-
-
     }
+
+    public function login(Request $request){
+        $request->validate([
+            "email"=>"required|email",
+            "password"=>"required",
+        ]);
+
+        if(!Auth::attempt($request->only("email","password"))){
+            return response()->json(["success"=>false,"message"=>"Email and Password does not match!"]);
+        }
+
+        $user=User::where("email",$request["email"])->firstOrFail();
+
+        $token=$user->createToken("Token-Name")->plainTextToken;
+
+        return response()->json([
+            "success"=>true,
+            "user"=>$user,
+            "token"=>$token,
+        ]);
+    }
+    
 
 
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, "register"]);
 Route::post('logout', [AuthController::class, "logout"]);
+Route::post('login', [AuthController::class, "login"]);
